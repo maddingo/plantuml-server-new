ea044b9dbeee92822cacaf6a2006e1c0d9f3afbe
Martin Goldhahn <mgoldhahn@gmail.com>
2020-03-02 23:17:04 +0100

    use htmlunit for testing, since httpunit cannot cope with Javascript, copied plantuml servlet

--- plantuml-web/src/test/java/no/maddin/plantuml/IndexPageIT.java
+++ plantuml-web/src/test/java/no/maddin/plantuml/IndexPageIT.java
@@ -1,28 +1,22 @@
 package no.maddin.plantuml;
 
-import com.meterware.httpunit.GetMethodWebRequest;
-import com.meterware.httpunit.WebConversation;
-import com.meterware.httpunit.WebRequest;
-import com.meterware.httpunit.WebResponse;
-import org.junit.ClassRule;
+import com.gargoylesoftware.htmlunit.WebClient;
+import com.gargoylesoftware.htmlunit.html.HtmlHeading1;
+import com.gargoylesoftware.htmlunit.html.HtmlPage;
 import org.junit.Test;
 
-import java.net.URI;
-
-import static org.hamcrest.Matchers.containsString;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.hasProperty;
 import static org.junit.Assert.assertThat;
 
 public class IndexPageIT {
 
-    @ClassRule
-    public static JettyServerRule jettyServer = JettyServerRule.create(8888, "target/plantuml-web.war");
-
     @Test
     public void indexPage() throws Exception {
-        URI serverUri = jettyServer.getServer().getURI();
-        WebConversation wc = new WebConversation();
-        WebRequest wr = new GetMethodWebRequest(serverUri.toString());
-        WebResponse response = wc.getResponse(wr);
-        assertThat(response.getText(), containsString("PlantUML"));
+        try (WebClient webClient = new WebClient()) {
+            HtmlPage indexPage = webClient.getPage(System.getProperty("app.url"));
+            final HtmlHeading1 h1 = (HtmlHeading1) indexPage.getByXPath("//div[@id='header']/h1").get(0);
+            assertThat(h1, hasProperty("textContent", equalTo("PlantUML Server")));
+        }
     }
 }
