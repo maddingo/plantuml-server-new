8befb712c9baa02db2b70ad9315e608a15a37e22
Martin Goldhahn <mgoldhahn@gmail.com>
2020-03-03 01:25:20 +0100

    more checks on indexpage

--- plantuml-web/src/test/java/no/maddin/plantuml/IndexPageIT.java
+++ plantuml-web/src/test/java/no/maddin/plantuml/IndexPageIT.java
@@ -1,9 +1,7 @@
 package no.maddin.plantuml;
 
 import com.gargoylesoftware.htmlunit.WebClient;
-import com.gargoylesoftware.htmlunit.html.HtmlHeading1;
-import com.gargoylesoftware.htmlunit.html.HtmlPage;
-import com.gargoylesoftware.htmlunit.html.HtmlTextArea;
+import com.gargoylesoftware.htmlunit.html.*;
 import org.junit.Test;
 
 import static org.hamcrest.Matchers.*;
@@ -14,13 +12,17 @@ public class IndexPageIT {
     @Test
     public void indexPage() throws Exception {
         try (WebClient webClient = new WebClient()) {
-            HtmlPage indexPage = webClient.getPage(System.getProperty("app.url"));
+            String appUrl = System.getProperty("app.url");
+            HtmlPage indexPage = webClient.getPage(appUrl);
             final HtmlHeading1 h1 = indexPage.getFirstByXPath("//div[@id='header']/h1");
             assertThat(h1, hasProperty("textContent", equalTo("PlantUML Server")));
 
             HtmlTextArea textArea = indexPage.getFirstByXPath("//textarea[@id='text']");
             assertThat(textArea, hasProperty("displayed", equalTo(false)));
             assertThat(textArea, hasProperty("text", containsString("@startuml")));
+
+            HtmlInput input = indexPage.getFirstByXPath("//input[@name='url']");
+            assertThat(input, hasProperty("valueAttribute", startsWith(appUrl)));
         }
     }
 }
