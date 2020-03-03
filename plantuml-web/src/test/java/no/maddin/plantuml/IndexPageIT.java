7e2659a34ab6ae7d60a01a1269a4353b6a903319
Martin Goldhahn <mgoldhahn@gmail.com>
2020-03-03 01:19:22 +0100

    more checks on indexpage

--- plantuml-web/src/test/java/no/maddin/plantuml/IndexPageIT.java
+++ plantuml-web/src/test/java/no/maddin/plantuml/IndexPageIT.java
@@ -3,10 +3,10 @@ package no.maddin.plantuml;
 import com.gargoylesoftware.htmlunit.WebClient;
 import com.gargoylesoftware.htmlunit.html.HtmlHeading1;
 import com.gargoylesoftware.htmlunit.html.HtmlPage;
+import com.gargoylesoftware.htmlunit.html.HtmlTextArea;
 import org.junit.Test;
 
-import static org.hamcrest.Matchers.equalTo;
-import static org.hamcrest.Matchers.hasProperty;
+import static org.hamcrest.Matchers.*;
 import static org.junit.Assert.assertThat;
 
 public class IndexPageIT {
@@ -15,8 +15,12 @@ public class IndexPageIT {
     public void indexPage() throws Exception {
         try (WebClient webClient = new WebClient()) {
             HtmlPage indexPage = webClient.getPage(System.getProperty("app.url"));
-            final HtmlHeading1 h1 = (HtmlHeading1) indexPage.getByXPath("//div[@id='header']/h1").get(0);
+            final HtmlHeading1 h1 = indexPage.getFirstByXPath("//div[@id='header']/h1");
             assertThat(h1, hasProperty("textContent", equalTo("PlantUML Server")));
+
+            HtmlTextArea textArea = indexPage.getFirstByXPath("//textarea[@id='text']");
+            assertThat(textArea, hasProperty("displayed", equalTo(false)));
+            assertThat(textArea, hasProperty("text", containsString("@startuml")));
         }
     }
 }
